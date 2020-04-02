e006d2f6fbee9d940c7027f5f01bb11c24d151b0
bobweek <bobweek@gmail.com>
2020-04-02 01:25:56 -0700

    fig4Updated!

--- sde_num_sol.jl
+++ sde_num_sol.jl
@@ -4,9 +4,13 @@ using Parameters, Statistics, Random, LinearAlgebra, Distributions,
 include("/home/bb/Gits/branching.brownian.motion.and.spde/sde_functions.jl")
 
 j=1
-k=2
+k=1
 # strengths of competition
 cc = [1e-7,5e-6]
+
+# for S = 1000
+cc = [1e-8,5e-7]
+
 
 # durations
 TT = [1e3,5e3]
@@ -14,7 +18,7 @@ TT = [1e3,5e3]
 tspan = (0.0,TT[j])
 
 # background parameters
-S = 100
+S = 1000
 w = fill(0.1, S)  # niche breadths
 U = fill(1.0, S)  # total niche use
 c = fill(cc[k],S)      # strengths of competition
@@ -43,8 +47,6 @@ u₀ = cat(θ,fill(10.0, S),fill(1000.0, S),dims=1)
 prob = SDEProblem(f,g,u₀,tspan,pars)
 sol = solve(prob)
 
-plot(sol,ylim=(-10,10))
-
 # extract state variables
 x̄_sol = copy(.-sol[(0*S+1):(1*S),:])
 G_sol = copy(sol[(1*S+1):(2*S),:])
@@ -64,4 +66,30 @@ end
 #CSV.write("/home/bob/Research/Branching Brownian Motion/sample_path_wc.csv", df)
 
 # moderate competition
-CSV.write("/home/bob/Research/Branching Brownian Motion/sample_path_mc.csv", df)
+#CSV.write("/home/bob/Research/Branching Brownian Motion/sample_path_mc.csv", df)
+
+# for S = 1000
+
+LT = length(sol)
+
+# extract state variables
+x̄_sol = copy(.-sol[(0*S+1):(1*S),LT])
+G_sol = copy(sol[(1*S+1):(2*S),LT])
+N_sol = copy(sol[(2*S+1):(3*S),LT])
+
+# build dataframes
+spp = string("Species", 1)
+df = DataFrame(spp = spp, x = x̄_sol[1], G = G_sol[1], N = N_sol[1])
+for i in 2:S
+	spp = string("Species", i)
+	df_dummy = DataFrame(spp = spp, x = x̄_sol[i], G = G_sol[i],
+	N = N_sol[i])
+	global df = append!(df,df_dummy)
+end
+
+
+# weak competition
+CSV.write("/home/bb/Gits/branching.brownian.motion.and.spde/sample_path_wc1e3.csv", df)
+
+# moderate competition
+#CSV.write("/home/bb/Gits/branching.brownian.motion.and.spde/sample_path_mc1e3.csv", df)
