d532be2e20fac60aaf64418fe8101fa922a455e9
bobweek <bobweek@gmail.com>
2020-08-10 09:44:45 -0700

    spde

--- spde.jl
+++ spde.jl
@@ -1,52 +1,91 @@
-"""
-Solve the heat equation ∂ₜu = Δu on [0,1] using OrdinaryDiffEq for time stepping.
-Boundary conditions are such that an analytic solution is given by:
-    sin(2π*x) * exp(-(2*π)^2*t)
-This example demonstrates how to solve time dependent PDE's combining `fenics` and `DiffEq`.
-"""
-#module HeatEquationExample
+#
+# Adapted from
+#
+# http://www.stochasticlifestyle.com/solving-systems-stochastic-pdes-using-gpus-julia/
+#
 
-using Test
-using OrdinaryDiffEq
-using FEniCS
+using OrdinaryDiffEq, StochasticDiffEq, RecursiveArrayTools, LinearAlgebra, Plots, Parameters
 
-mesh = UnitIntervalMesh(50)
-V = FunctionSpace(mesh,"P",1)
-u_str = "sin(2*pi*x[0]) * exp(-t*pow(2*pi, 2))"
+const N = 100
 
-tspan = (0.0, 0.1)
-u = interpolate(Expression(u_str, degree=2, pi=Float64(pi), t=tspan[1]), V)
+const Mx = Tridiagonal([1.0 for i in 1:N-1],[-2.0 for i in 1:N],[1.0 for i in 1:N-1])
 
-bc = DirichletBC(V, 0.0, "on_boundary")
-dudt = TrialFunction(V)
-v = TestFunction(V)
-Dudt = FeFunction(V)
-F = dot(dudt, v)*dx + dot(grad(u), grad(v))*dx
-a = lhs(F)
-L = rhs(F)
-p = (u=u, Dudt=Dudt, bc=bc, a=a, L=L)
-
-function f!(dudt_vec, u_vec, p, t)
-    assign(p.u, u_vec)
-    lvsolve(p.a, p.L, p.Dudt, p.bc)
-    copy!(dudt_vec, get_array(p.Dudt))
+# data type that holds model parameters
+@with_kw mutable struct ModelParameters
+    μ::Float64	# mutation rate
+    R::Float64	# intrinsic rates of growth
+    a::Float64	# strength of abiotic selection
+    θ::Float64	# phenotypic optima
+	c::Float64	# strength of competition
+	V::Float64	# variance of reproductive output
+    X::Vector{Float64}	# variance of reproductive output
 end
 
-u0_vec = get_array(u)
-t_final = 0.1
-prob = ODEProblem(f!, u0_vec, tspan, p)
-u_true = interpolate(Expression(u_str, degree=2, pi=Float64(pi), t=tspan[2]), V)
+# Define the constants for the PDE
+θ = 30.0
+ω = 100.0
+c = 0.001
+a = 0.001
+μ = 0.1
+V = 1
+R = 1
+X = Vector([i for i in 1:N])
 
-# some algorithms to try:
-# alg = ImplicitEuler(autodiff=false)
-# alg = Rosenbrock23(autodiff=false)
-# alg = Rodas5(autodiff=false)
+pars = ModelParameters(μ=μ, R=R, a=a, θ=θ, c=c, V=V, X=X)
 
-alg = KenCarp4(autodiff=false)
-sol = OrdinaryDiffEq.solve(prob, alg, reltol=1e-6, abstol=1e-6)
+# Define the initial condition as normal arrays
+u0 = 1000*exp.(-(X.-50).^2 / (2*ω))/√(2*π*ω)
 
-vec_sol = get_array(p.u)
-vec_true = get_array(u_true)
-@test vec_sol ≈ vec_true rtol=1e-2
+# Define the discretized PDE as an ODE function
+function f(u,p,t)
+    @unpack μ, R, a, θ, c = p
+    du = 0.5*μ*Mx*u + u.*(R .- 0.5*a*(X.-θ).^2 .- c*norm(u,1))
+    return(du)
+end
 
-#end#module
+function g(u,p,t)
+    @unpack V, X = p
+    du = zeros(length(X))
+    for i in 1:length(X)
+        if u[i]>0
+            du[i] = √(V*u[i])
+        end
+    end
+    return(du)
+end
+
+#######################################################
+### Solve the PDE
+#######################################################
+
+prob = ODEProblem(f,u0,(0.0,50.0),pars)
+sol = solve(prob,ROCK2())
+
+time = sol.t
+
+abun = zeros(N,length(time))
+for i in 1:length(time)
+    MAX = maximum(sol.u[i])
+    for j in 1:N
+        abun[j,i] = sol.u[i][j]
+    end
+end
+contour(time,X,abun,fill = true)
+
+#######################################################
+### Solve the SPDE
+#######################################################
+
+prob2 = SDEProblem(f,g,u0,(0.0,50.0),pars)
+sol = solve(prob2,SOSRI())
+
+time = sol.t
+
+abun = zeros(N,length(time))
+for i in 1:length(time)
+    MAX = maximum(sol.u[i])
+    for j in 1:N
+        abun[j,i] = sol.u[i][j]
+    end
+end
+contour(time,X,abun,fill = true)
